f5b611594c3e38208a8ced6c13e035769c1d6eb4
misha-solntcev <misha.solntcev@gmail.com>
2026-06-11 00:26:34 +0800

    refactor: remove obsolete merge_duplicates script
    - Deleted the entire merge_duplicates.py script, which was used for merging duplicate records in the database.
    - The script handled merging op.media records, updating related tables, and renumbering media units.
    - It's been removed as it's no longer needed for current operations.

--- run_merge.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""Запуск merge_duplicates.py через Odoo shell."""
-import odoo
-from odoo.tools import config
-
-config.parse_config(['-c', '/etc/odoo/odoo.conf', '-d', 'test4'])
-
-with odoo.service.server.start(preload=['test4']) as env:
-    exec(open('/mnt/extra-addons/merge_duplicates.py').read())
