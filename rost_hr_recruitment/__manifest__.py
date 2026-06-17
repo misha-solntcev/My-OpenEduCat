529e5f204c1e6ac752c37a29f859f9bc37e2d94b
misha-solntcev <misha.solntcev@gmail.com>
2026-06-17 12:07:16 +0800

    feat(rost_hr_recruitment): Добавлен новый модуль который меняет шаблон веб-страницы для описания школы при подборе персонала.

---
+++ rost_hr_recruitment/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Rost School Recruitment Customization',
+    'version': '1.0',
+    'category': 'Website/Website',
+    'summary': 'Адаптация шаблонов вакансий для частной школы РОСТ',
+    'author': 'Частная школа РОСТ',
+    'website': 'https://rostschoolspb.ru',
+    'depends': ['website_hr_recruitment'],
+    'data': [
+        'views/website_hr_recruitment_templates.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
